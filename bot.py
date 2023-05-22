b765c4b938246017cfc59eee9c5a4400739ccd65
Banana <youlovemybanana@gmail.com>
2023-05-22 14:04:21 +0330

    log /start

--- bot.py
+++ bot.py
@@ -39,7 +39,10 @@ else:
 @bot.on(events.NewMessage(pattern='/start', incoming=True))
 @bot.on(events.CallbackQuery(data=b'main_menu'))
 async def start(event):
-    print(event.sender_id)
+    try:
+        print(event.message)
+    except:
+        pass
     if event.sender_id in config.admin_list:
         welcome_msg = reporting.welcome_admin(db, msg, config)
         k_admin = helper.get_start_admin_buttons(msg, config)
